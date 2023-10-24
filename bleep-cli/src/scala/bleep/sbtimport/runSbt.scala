9f0dc3556989553e227c13468c62955845ee10ee
Kristian Nedrevold-Hansen <kristian@krined.no>
2023-10-25 00:07:54 +0200

    formatting

--- bleep-cli/src/scala/bleep/sbtimport/runSbt.scala
+++ bleep-cli/src/scala/bleep/sbtimport/runSbt.scala
@@ -266,5 +266,4 @@ object runSbt {
       new ScalaVersionOutput(scalaVersions, crossVersions)
     }
   }
-
 }
---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "bleep";
+
+  inputs = {
+     nixpkgs.url = "github:NixOS/nixpkgs";
+     bleepSrc.url = "github:KristianAN/bleep-flake"; # The bleep flake
+     flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, bleepSrc, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+          bleep = bleepSrc.defaultPackage.${system}; # Your bleep system binary
+
+          jdk = pkgs.jdk17_headless;
+
+          jvmHook = ''
+            JAVA_HOME="${jdk}"
+          '';
+      in {
+        devShell = pkgs.mkShell rec {
+          buildInputs = [
+            bleep # Bleep bleep
+            jdk
+          ];
+          shellHook = jvmHook;          
+        };
+      });
+}
